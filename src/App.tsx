24d6cc9df567a7f1d9592b8d0c35febddd12316f
Trevor <tkoenig@udel.edu>
2023-02-13 14:35:54 -0500

    added header

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-            <p>by Trevor</p>
+            <h1>by Trevor</h1>
             <p>Hello World!</p>
             <br></br>
         </div>
