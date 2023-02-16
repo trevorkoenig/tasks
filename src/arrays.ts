804f22d2b136404942e7187d342f1afe41e1700a
Trevor <tkoenig@udel.edu>
2023-02-16 16:25:59 -0500

    completed first three array functions

--- src/arrays.ts
+++ src/arrays.ts
@@ -5,6 +5,10 @@
  * the number twice.
  */
 export function bookEndList(numbers: number[]): number[] {
+    if (numbers.length === 0) {
+        return [];
+    }
+    numbers = [numbers[0], numbers[numbers.length - 1]];
     return numbers;
 }
 
@@ -13,7 +17,8 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
+    const tripled = numbers.map((num: number): number => num * 3);
+    return tripled;
 }
 
 /**
@@ -21,7 +26,10 @@ export function tripleNumbers(numbers: number[]): number[] {
  * the number cannot be parsed as an integer, convert it to 0 instead.
  */
 export function stringsToIntegers(numbers: string[]): number[] {
-    return [];
+    const ints: number[] = numbers.map((str: string): number =>
+        !isNaN(parseInt(str)) ? parseInt(str) : 0
+    );
+    return ints;
 }
 
 /**
