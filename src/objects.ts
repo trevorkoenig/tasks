9f90e6c40b45500f84da22bf1dd1a51b609ad1ec
Trevor <tkoenig@udel.edu>
2023-02-22 15:08:53 -0500

    finished toMarkdown

--- src/objects.ts
+++ src/objects.ts
@@ -79,7 +79,15 @@ export function toShortForm(question: Question): string {
  * Check the unit tests for more examples of what this looks like!
  */
 export function toMarkdown(question: Question): string {
-    return "";
+    if (question.type === "multiple_choice_question") {
+        const optionstring: string = "-" + question.options.join("\n-");
+        return `# ${question.name}
+${question.body}
+${optionstring}`;
+    } else {
+        return `# ${question.name}
+${question.body}`;
+    }
 }
 
 /**
