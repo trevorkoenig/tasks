8215e7fcfd76571ab169db4603dc0d55ac591e06
Trevor <tkoenig@udel.edu>
2023-03-06 12:51:36 -0500

    finished revealAnswer

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,13 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [answerVisibility, setAnswerVisibility] = useState<boolean>(false);
+    return (
+        <div>
+            <Button onClick={() => setAnswerVisibility(!answerVisibility)}>
+                {answerVisibility ? "Hide Answer" : "Show Answer"}
+            </Button>
+            <p>Answer: {answerVisibility && <span>42</span>}</p>
+        </div>
+    );
 }
