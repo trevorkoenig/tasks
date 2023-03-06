cdc552237aa919a159c73ee3fd6313fb50f1ea0c
Trevor <tkoenig@udel.edu>
2023-03-06 14:22:03 -0500

    prettier is fucking up my code

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -2,5 +2,26 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function CycleHoliday(): JSX.Element {
+    type holiday =
+        | "Christmas"
+        | "Earth Day"
+        | "Halloween"
+        | "New Years Eve"
+        | "Thanksgiving";
+    const alphabetical: Record<holiday, holiday> = {
+        Christmas: "Earth Day",
+        "Earth Day": "Halloween",
+        Halloween: "New Years Eve",
+        "New Years Eve": "Thanksgiving",
+        Thanksgiving: "Christmas"
+    };
+    const chronological: Record<holiday, holiday> = {
+        "Earth Day": "Halloween",
+        Halloween: "Thanksgiving",
+        Thanksgiving: "Christmas",
+        Christmas: "New Years Eve",
+        "New Years Eve": "Earth Day"
+    };
+
     return <div>Cycle Holiday</div>;
 }
