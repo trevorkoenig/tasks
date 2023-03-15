6573513909b0cf87a14dda02459955b6e0d61ba1
Trevor <tkoenig@udel.edu>
2023-03-15 16:09:27 -0400

    fixed DoubleHalf with props, passes all tests

--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -1,23 +1,29 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+interface dhProps {
+    dhValue: number;
+    setDhValue: (newDhValue: number) => void;
+}
+
+function Doubler({ dhValue, setDhValue }: dhProps): JSX.Element {
+    return <Button onClick={() => setDhValue(2 * dhValue)}>Double</Button>;
+}
+
+function Halver({ dhValue, setDhValue }: dhProps): JSX.Element {
+    return <Button onClick={() => setDhValue(0.5 * dhValue)}>Halve</Button>;
+}
+
 export function DoubleHalf(): JSX.Element {
     const [dhValue, setDhValue] = useState<number>(10);
-
-    function Doubler(): JSX.Element {
-        return <Button onClick={() => setDhValue(2 * dhValue)}>Double</Button>;
-    }
-    function Halver(): JSX.Element {
-        return <Button onClick={() => setDhValue(0.5 * dhValue)}>Halve</Button>;
-    }
     return (
         <div>
             <h3>Double Half</h3>
             <div>
                 The current value is: <span>{dhValue}</span>
             </div>
-            <Doubler></Doubler>
-            <Halver></Halver>
+            <Doubler dhValue={dhValue} setDhValue={setDhValue}></Doubler>
+            <Halver dhValue={dhValue} setDhValue={setDhValue}></Halver>
         </div>
     );
 }
