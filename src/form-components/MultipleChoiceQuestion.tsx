6562a1f504e386fbc8f1f82744a120f43bf52406
Trevor <tkoenig@udel.edu>
2023-03-20 18:16:18 -0400

    finished, centered, passed all tests

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Col, Container, Form, Row } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,33 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [answer, setAnswer] = useState<string>(options[0]);
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Container style={{ justifyContent: "center", display: "flex" }}>
+                <Row sm="auto">
+                    <Form.Group controlId="answersDropdown">
+                        <Form.Label>Select an answer</Form.Label>
+                        <Col>
+                            <Form.Select
+                                value={answer}
+                                onChange={(e) => setAnswer(e.target.value)}
+                            >
+                                {options.map(
+                                    (o: string): JSX.Element => (
+                                        <option key={o} value={o}>
+                                            {o}
+                                        </option>
+                                    )
+                                )}
+                            </Form.Select>
+                            <br></br>
+                            {answer === expectedAnswer ? "✔️" : "❌"}
+                        </Col>
+                    </Form.Group>
+                </Row>
+            </Container>
         </div>
     );
 }
