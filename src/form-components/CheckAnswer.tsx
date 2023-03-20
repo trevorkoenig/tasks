49f44bb52edd06388093314da6be8818473bdafa
Trevor <tkoenig@udel.edu>
2023-03-20 13:24:21 -0400

    checkAnswer functional, not sure if passed

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,31 @@
 import React, { useState } from "react";
+import { Form, Row, Col } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [correct, setCorrect] = useState<boolean>(false);
+
+    function checkAnswer(event: React.ChangeEvent<HTMLInputElement>) {
+        setCorrect(event.target.value === expectedAnswer);
+    }
+
     return (
         <div>
             <h3>Check Answer</h3>
+            <Form.Group controlId="guess" as={Row}>
+                <Form.Label column sm={2}>
+                    Answer
+                </Form.Label>
+                <Col sm={8}>
+                    <Form.Control type="text" onChange={checkAnswer} />
+                </Col>
+                <Col sm={2}>
+                    <div>{correct ? "✔️" : "❌"}</div>
+                </Col>
+            </Form.Group>
         </div>
     );
 }
