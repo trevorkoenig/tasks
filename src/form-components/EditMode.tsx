f463fb3890897cb940828d1acd0914e240c9e239
Trevor <tkoenig@udel.edu>
2023-03-20 16:50:19 -0400

    editmode fully kosher, passed all tests

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,56 @@
 import React, { useState } from "react";
+import { Col, Container, Form, Row } from "react-bootstrap";
 
 export function EditMode(): JSX.Element {
+    const [editMode, setEditMode] = useState<boolean>(false);
+    const [isStudent, setIsStudent] = useState<boolean>(true);
+    const [input, setInput] = useState<string>("Your Name");
+
     return (
         <div>
             <h3>Edit Mode</h3>
+            <Container>
+                <Row>
+                    <Col>
+                        <Form.Check
+                            type="switch"
+                            id="is-edit-mode"
+                            label="Edit Mode"
+                            checked={editMode}
+                            onChange={(
+                                event: React.ChangeEvent<HTMLInputElement>
+                            ) => setEditMode(event.target.checked)}
+                        />
+                    </Col>
+                    <Col>
+                        {editMode ? (
+                            <Form.Group controlId="guess" as={Row}>
+                                <Form.Control
+                                    type="text"
+                                    value={input}
+                                    onChange={(
+                                        event: React.ChangeEvent<HTMLInputElement>
+                                    ) => setInput(event.target.value)}
+                                />
+                                <Row sm="auto">
+                                    <Form.Check
+                                        id="is-a-student"
+                                        label=" Is a student?"
+                                        checked={isStudent}
+                                        onChange={(
+                                            event: React.ChangeEvent<HTMLInputElement>
+                                        ) => setIsStudent(event.target.checked)}
+                                    />
+                                </Row>
+                            </Form.Group>
+                        ) : (
+                            <p>
+                                {input} is {isStudent ? "" : "not"} a student
+                            </p>
+                        )}
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
